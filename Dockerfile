96edcf27057214818014e96c35288c5423d1e988
Az Builds <RadioAndrea@users.noreply.github.com>
2024-02-25 10:12:28 -0500

    Fixed gid/uid issues

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,19 @@
-FROM python:3.11
+# Pull python 3.11 because that is our latest target
+FROM python:3.11-slim
 
-RUN groupadd -r freetak && useradd -m -r -g freetak freetak
-
-RUN mkdir -p /home/freetak/data && chown -R freetak:freetak /home/freetak/data && chmod 777 -R /home/freetak/data && chmod g+s /home/freetak/data
-
+# Add non-root user to limit privilege to minimum required to function
+#RUN adduser --disabled-password --home /home/freetak freetak
+RUN useradd --system --create-home --shell /bin/bash freetak
 USER freetak
-WORKDIR /home/freetak
 
-# Install pre-reqs then the base FTS
+# Move to non-root user's home directory as that should be the only place it has permission
+WORKDIR /home/freetak/
 ENV PATH /home/freetak/.local/bin:/home/freetak/.local/lib:$PATH
 
 COPY --chown=freetak:freetak --chmod=774 . ./
-RUN pwd && ls
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --ignore-installed PyYAML --no-cache-dir -r requirements.txt
 RUN pip install -e .
 
-EXPOSE 5000/tcp
+# When container is run, run our startup script
 CMD ["/home/freetak/docker-run.sh"]
--- docker-run.sh
+++ docker-run.sh
@@ -4,26 +4,18 @@
 # Some systems use `python3` instead of `python` so this is not entirely portable
 cd "/home/freetak/FreeTAKServer-UI/" || raise error "Could not navigate to the user-sites path. Are you using a distro that requires python3 instead of python?"
 
-# OBE as this is handled in the dockerfile, could be helpful in other kinds of deployments
-# If config.py exists
-#if test -f config.py; then
-  # Then we need to move it to a "backup"
-  # this way we can always give the user a new one if they delete theirs
-#  mv config.py config.bak
+## Check if there is *NOT* a config.py in the shared volume
+#if [[ ! -f "/home/freetak/data/config.py" ]]
+#  then
+#    # If there isn't, then we need to give one to the user
+#    cp config.bak /home/freetak/data/config.py
 #fi
 
-# Check if there is *NOT* a config.py in the shared volume
-if [[ ! -f "/home/freetak/data/config.py" ]]
-  then
-    # If there isn't, then we need to give one to the user
-    cp config.bak /home/freetak/data/config.py
-fi
-
-# If the symlink has not been created yet, then we will do that
-if [[ ! -f "config.py" ]]
-  then
-    ln -s /home/freetak/data/config.py config.py
-fi
+## If the symlink has not been created yet, then we will do that
+#if [[ ! -f "config.py" ]]
+#  then
+#    ln -s /home/freetak/data/config.py config.py
+#fi
 
 # TODO This can be implemented once the FTS-UI has an SSL operation mode
 # Check if SSL certificates in the shared volumes exists
