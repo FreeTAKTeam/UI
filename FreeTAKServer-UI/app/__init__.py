8d5028f5007145d531aced87f063b1ce0ee0188e
Natha Paquette <natha.paquette@gmail.com>
2024-03-30 13:33:41 -0300

    Update __init__.py

--- FreeTAKServer-UI/app/__init__.py
+++ FreeTAKServer-UI/app/__init__.py
@@ -88,7 +88,7 @@ def create_app(config, selenium=False):
     # UI configuration
     # UI version DO NOT modify it
     
-    app.config['UIVERSION'] = '2.1'
+    app.config['UIVERSION'] = '2.1.1.1'
 
     # number of milliseconds to query FTS for connected Users, health, a System status
     app.config['USERINTERVAL'] = '180000';
